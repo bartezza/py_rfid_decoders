64c96956149c0f59fb1c58ccabe1ea15ca16f943
Andrea Bartezzaghi <andrea.bartezzaghi@gmail.com>
2021-07-11 17:59:22 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="rfid_decoders",
     version="0.1",
     description="RFID decoders from raw bitstream",
-    url="https://github.com/bartezza/rfid_decoders",
+    url="https://github.com/bartezza/py_rfid_decoders",
     author="Andrea Bartezzaghi",
     author_email="andrea.bartezzaghi@gmail.com",
     license="MIT",
